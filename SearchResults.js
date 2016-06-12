05a80ff03d5db32b520337fd56c56ae548855607
Christian Fleschhut <weltrekordhalter@gmail.com>
2016-06-12 22:09:39 +0200

    style search result listview items

--- SearchResults.js
+++ SearchResults.js
@@ -10,6 +10,34 @@ import {
   ListView
 } from 'react-native';
 
+const styles = StyleSheet.create({
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  },
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#ddd'
+  }
+});
+
 export default class SearchResults extends Component {
   constructor(props) {
     super(props);
@@ -27,7 +55,19 @@ export default class SearchResults extends Component {
     return (
       <TouchableHighlight underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{uri: rowData.img_url}} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>
+                {rowData.price_formatted}
+              </Text>
+              <Text style={styles.title}
+                numberOfLines={1}>
+                {rowData.title}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
         </View>
       </TouchableHighlight>
     );
